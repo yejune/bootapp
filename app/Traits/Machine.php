0c59b283c0da3cc318b4cf9c0a9942b5a89598e9
yejune <k@yejune.com>
2019-06-15 15:18:14 +0900

    build 0.2.77

--- app/Traits/Machine.php
+++ app/Traits/Machine.php
@@ -640,24 +640,12 @@ trait Machine
                 foreach (['devel.host', 'mockup.host', 'mockin.host', 'resolv.host', 'faked.host'] as $checkDomain) {
                     if ($domain == $checkDomain || preg_match('#'.addcslashes($checkDomain, '.').'$#', $domain)) {
                         if (true === file_exists($certPemfile)) {
-                            if (0 === strpos(\Phar::running(), 'phar://')) {
-                                copy('phar://bootapp.phar/certs/'.$checkDomain.'.crt', $certPemfile);
-                                $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'install ./var/certs/'.$domain.'.crt');
-                            } else {
-                                copy(__DIR__.'/../../certs/'.$checkDomain.'.crt', $certPemfile);
-                                $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'exists ./var/certs/'.$domain.'.crt');
-                            }
+                            $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'exists ./var/certs/'.$domain.'.key');
+                        } else {
+                            copy(__DIR__.'/../../certs/'.$checkDomain.'.pem', $certPemfile);
+                            copy(__DIR__.'/../../certs/'.$checkDomain.'.key', $certKeyFile);
+                            $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'install ./var/certs/'.$domain.'.key');
                         }
-                        if (true === file_exists($certKeyFile)) {
-                            if (0 === strpos(\Phar::running(), 'phar://')) {
-                                copy('phar://bootapp.phar/certs/'.$checkDomain.'.key', $certKeyFile);
-                                $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'install ./var/certs/'.$domain.'.crt');
-                            } else {
-                                copy(__DIR__.'/../../certs/'.$checkDomain.'.key', $certKeyFile);
-                                $this->message(\Peanut\Console\Color::gettext('        | ', 'white').'exists ./var/certs/'.$domain.'.crt');
-                            }
-                        }
-
                         continue;
                     }
                 }
