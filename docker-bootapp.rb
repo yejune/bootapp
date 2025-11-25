6275bc7c10d7d4eb294685b4f3a28f7afd6094e1
권승민 <max@superconnect.co.kr>
2025-11-25 13:57:42 +0900

    feat: update Homebrew formula with SHA256 and standalone binary
    - Add SHA256 hash for v1.0.0 release
    - Install symlink for standalone 'bootapp' command
    - Update caveats with both usage methods
    - Clarify platform-specific requirements (OrbStack/Linux vs Docker Desktop/Colima)
    - Update tests to verify both binaries

--- docker-bootapp.rb
+++ docker-bootapp.rb
@@ -2,7 +2,7 @@ class DockerBootapp < Formula
   desc "Docker CLI Plugin for multi-project Docker networking made easy"
   homepage "https://github.com/yejune/docker-bootapp"
   url "https://github.com/yejune/docker-bootapp/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # Will be updated when you create a release
+  sha256 "503cbab3bb70a539c2d30f8636af14995168a0ae6fff8649834fdb6cf6258ab4"
   license "MIT"
   head "https://github.com/yejune/docker-bootapp.git", branch: "main"
 
@@ -17,26 +17,36 @@ class DockerBootapp < Formula
     mkdir_p docker_plugins
     cp bin/"docker-bootapp", "#{docker_plugins}/docker-bootapp"
     chmod 0755, "#{docker_plugins}/docker-bootapp"
+
+    # Also install as standalone 'bootapp' binary
+    bin.install_symlink "docker-bootapp" => "bootapp"
   end
 
   def caveats
     <<~EOS
-      docker-bootapp has been installed as a Docker CLI plugin!
+      docker-bootapp has been installed!
+
+      You can use it in two ways:
+        docker bootapp [command]  # As Docker CLI plugin
+        bootapp [command]         # As standalone binary
 
       Usage:
         docker bootapp up      # Start containers with auto-networking
         docker bootapp down    # Stop containers
         docker bootapp ls      # List managed projects
 
-      On macOS, docker-mac-net-connect is required for direct container IP access:
+      On macOS with Docker Desktop/Colima, docker-mac-net-connect is recommended:
         brew install chipmk/tap/docker-mac-net-connect
         sudo brew services start docker-mac-net-connect
+
+      On macOS with OrbStack or Linux, no additional tools needed!
     EOS
   end
 
   test do
-    # Test that the binary works
-    assert_match "docker-bootapp", shell_output("#{bin}/docker-bootapp --help")
+    # Test that both binaries work
+    assert_match "bootapp", shell_output("#{bin}/docker-bootapp help")
+    assert_match "bootapp", shell_output("#{bin}/bootapp help")
 
     # Verify it's installed as a Docker plugin
     docker_plugin = "#{ENV["HOME"]}/.docker/cli-plugins/docker-bootapp"
